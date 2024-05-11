f6ed284dd7851bb987b8811d4ab94c0435844cf1
sushant607 <svbagul_b22@ce.vjti.ac.in>
2024-05-11 23:13:58 +0530

    mongodb connected

--- backend/routes/signup.js
+++ backend/routes/signup.js
@@ -2,9 +2,9 @@
 // SjANOXACErKMbtta
 const express = require('express');
 const mongoose = require('mongoose');
+// const signupRouter = require('./routes/signupRouter'); // Import the signup router
 const app = express();
 const port = 3000;
-import cors from "cors";
 mongoose.connect('mongodb+srv://sushantbagul607:SjANOXACErKMbtta@cluster0.2qjfhyy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
   useNewUrlParser: true,
   useUnifiedTopology: true
@@ -12,9 +12,4 @@ mongoose.connect('mongodb+srv://sushantbagul607:SjANOXACErKMbtta@cluster0.2qjfhy
   console.log("MongoDB connected");
 }).catch(err => {
   console.error("MongoDB connection error", err);
-});
-app.use(cors())
-app.use(express.json());
-app.listen(port, () => {
-  console.log(`Server is listening at http://localhost:${port}`);
 });
